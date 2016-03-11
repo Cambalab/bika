82455bffb43d5ed0d8436904ca84b77710d55306
José Luis Di Biase <josx@interorganic.com.ar>
2016-03-11 16:20:25 -0300

    Upgrade to ionic 2.0 beta
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -1,51 +1,138 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+/******************************************************************************
+ * Gulpfile
+ * Be sure to run `npm install` for `gulp` and the following tasks to be
+ * available from the command line. All tasks are run using `gulp taskName`.
+ ******************************************************************************/
+var gulp = require('gulp'),
+    webpack = require('webpack'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    watch = require('gulp-watch'),
+    del = require('del');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
-};
 
-gulp.task('default', ['sass']);
+var IONIC_DIR = "node_modules/ionic-angular/"
 
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
+
+/******************************************************************************
+ * watch
+ * Build the app and watch for source file changes.
+ ******************************************************************************/
+gulp.task('watch', ['sass', 'copy.fonts', 'copy.html'], function(done) {
+  watch('www/app/**/*.scss', function(){
+    gulp.start('sass');
+  });
+  watch('www/app/**/*.html', function(){
+    gulp.start('copy.html');
+  });
+  bundle(true, done);
+});
+
+
+/******************************************************************************
+ * build
+ * Build the app once, without watching for source file changes.
+ ******************************************************************************/
+gulp.task('build', ['sass', 'copy.fonts', 'copy.html'], function(done) {
+  bundle(false, done);
+});
+
+
+/******************************************************************************
+ * sass
+ * Convert Sass files to a single bundled CSS file. Uses auto-prefixer
+ * to automatically add required vendor prefixes when needed.
+ ******************************************************************************/
+gulp.task('sass', function(){
+  var autoprefixerOpts = {
+    browsers: [
+      'last 2 versions',
+      'iOS >= 7',
+      'Android >= 4',
+      'Explorer >= 10',
+      'ExplorerMobile >= 11'
+    ],
+    cascade: false
+  };
+
+  return gulp.src('app/theme/app.+(ios|md).scss')
+    .pipe(sass({
+      includePaths: [
+        IONIC_DIR,
+        'node_modules/ionicons/dist/scss'
+      ]
     }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    .on('error', function(err){
+      console.error(err.message);
+      this.emit('end');
+    })
+    .pipe(autoprefixer(autoprefixerOpts))
+    .pipe(gulp.dest('www/build/css'))
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+
+/******************************************************************************
+ * copy.fonts
+ * Copy Ionic font files to build directory.
+ ******************************************************************************/
+gulp.task('copy.fonts', function() {
+  return gulp.src(IONIC_DIR + 'fonts/**/*.+(ttf|woff|woff2)')
+    .pipe(gulp.dest('www/build/fonts'));
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+
+/******************************************************************************
+ * copy.html
+ * Copy html files to build directory.
+ ******************************************************************************/
+gulp.task('copy.html', function(){
+  return gulp.src('app/**/*.html')
+    .pipe(gulp.dest('www/build'));
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
+
+/******************************************************************************
+ * clean
+ * Delete previous build files.
+ ******************************************************************************/
+gulp.task('clean', function(done) {
+  del(['www/build'], done);
+});
+
+
+/******************************************************************************
+ * Bundle
+ * Transpiles source files and bundles them into build directory using webpack.
+ ******************************************************************************/
+function bundle(watch, cb) {
+  // prevent gulp calling done callback more than once when watching
+  var firstTime = true;
+
+  // load webpack config
+  var config = require('./webpack.config.js');
+
+  // https://github.com/webpack/docs/wiki/node.js-api#statstojsonoptions
+  var statsOptions = {
+    'colors': true,
+    'modules': false,
+    'chunks': false,
+    'exclude': ['node_modules']
   }
-  done();
-});
+
+  var compiler = webpack(config);
+  if (watch) {
+    compiler.watch(null, compileHandler);
+  } else {
+    compiler.run(compileHandler);
+  }
+
+  function compileHandler(err, stats){
+    if (firstTime) {
+      firstTime = false;
+      cb();
+    }
+
+    // print build stats and errors
+    console.log(stats.toString(statsOptions));
+  }
+}
