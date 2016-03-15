929cc586184e00d1755dd5a06515f8730d6c5c32
José Luis Di Biase <josx@interorganic.com.ar>
2016-03-15 11:51:54 -0300

    backend transitorio para probar
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

---
+++ frontend/server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    compression = require('compression'),
+    cors = require('cors'),
+    bicicleterias = require('./server/bicicleteria-service'),
+    app = express();
+
+app.set('port', process.env.PORT || 8000);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(compression());
+
+app.use('/', express.static(__dirname + '/www'));
+
+app.get('/bicicleterias', bicicleterias.findAll);
+app.get('/bicicleterias/:id', bicicleterias.findById);
+
+app.listen(app.get('port'), function () {
+    console.log('Realty server listening on port ' + app.get('port'));
+});
---
+++ frontend/server/mock-bicicleterias.js
@@ -0,0 +1,32 @@
+exports.data = [
+    {
+        id: 1,
+        name: "Santella",
+        desc: "La sabiduría que dan lo años de trabajo",
+    },
+    {
+        id: 2,
+        name: "Racer Bikes",
+        desc: "La última tecnología al servicio del pedal",
+    },
+    {
+        id: 3,
+        name: "Bici centro lanus",
+        desc: "El mejor precio del pueblo",
+    },
+    {
+        id: 4,
+        name: "Bicicleterias meo",
+        desc: "Rodados familiares y mini bicicletas",
+    },
+    {
+        id: 5,
+        name: "Compak Jet Rodados",
+        desc: "Venta de plegables y los mejores precios",
+    },
+    {
+        id: 6,
+        name: "Bicicleterias Tonino",
+        desc: "Bicics todo terreno y especialidades",
+    }
+];
