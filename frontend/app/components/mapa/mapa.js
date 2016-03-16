884886d94450de70d2f170d7b62535a076b86aba
José Luis Di Biase <josx@interorganic.com.ar>
2016-03-16 16:39:27 -0300

    Nuevo Componentes Mapas
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

---
+++ frontend/app/components/mapa/mapa.js
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+import {IONIC_DIRECTIVES} from 'ionic-angular';
+import {ANGULAR2_GOOGLE_MAPS_DIRECTIVES} from 'angular2-google-maps/core';
+
+@Component({
+  selector: 'mapa',
+  inputs: ['lat', 'lng', 'zoom', 'markers:markers'],
+  templateUrl: 'build/components/mapa/mapa.html',
+  directives: [IONIC_DIRECTIVES, ANGULAR2_GOOGLE_MAPS_DIRECTIVES],
+  styles: [`
+    .sebm-google-map-container {
+       height: 300px;
+     }
+  `],
+})
+export class Mapa {
+  constructor() {
+  }
+}
