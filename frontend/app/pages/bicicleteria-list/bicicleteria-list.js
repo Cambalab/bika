e3697673312d750e056d7cb509bb8976bfb0b78f
José Luis Di Biase <josx@interorganic.com.ar>
2016-03-18 15:21:02 -0300

    Poniendo en la page de listado el mapa
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- frontend/app/pages/bicicleteria-list/bicicleteria-list.js
+++ frontend/app/pages/bicicleteria-list/bicicleteria-list.js
@@ -1,10 +1,13 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
+import {Page, Component, NavController, NavParams} from 'ionic-angular';
 import {BicicleteriaDetailsPage} from '../bicicleteria-details/bicicleteria-details';
 import {BicicleteriaService} from '../../services/bicicleteria-service';
 import {Geolocation} from 'ionic-native';
+import {Mapa} from '../../components/mapa/mapa';
+import * as _ from 'lodash'
 
 @Page({
-  templateUrl: 'build/pages/bicicleteria-list/bicicleteria-list.html'
+  templateUrl: 'build/pages/bicicleteria-list/bicicleteria-list.html',
+  directives: [Mapa]
 })
 export class BicicleteriaListPage {
 
@@ -19,8 +22,9 @@ export class BicicleteriaListPage {
     this.bicicleteriaService = bicicleteriaService;
 
     Geolocation.getCurrentPosition().then((resp) => {
-       console.log("Latitude: ", resp.coords.latitude);
-       console.log("Longitude: ", resp.coords.longitude);
+      this.lng = resp.coords.longitude;
+      this.lat = resp.coords.latitude;
+      this.zoom = 15;
     })
   }
 
@@ -29,7 +33,31 @@ export class BicicleteriaListPage {
   }
 
   getBicicleterias() {
-    this.bicicleteriaService.findAll().subscribe(data => this.bicicleterias = data);
+    this.bicicleteriaService.findAll().subscribe(
+        data => {
+          this.bicicleterias = data;
+          this.markers = this.createMarkers(data,  {lat : 'lat', lng : 'lng', name : 'label'}, { title: 'name'} );
+        });
+  }
+
+  // Transforma datos de las bicicleterias
+  // data: json de bicicleterias, changes: cambios de nombre de keys,
+  // extend: agregar nuevas keys con datos que viene
+  createMarkers(data, changes, extend) {
+    let extendedData = _.map(data, item => {
+      let obj = _.mapValues(extend, value => { return item[value]; });
+      return _.extend({}, item, obj);
+    });
+
+    let extendKeys = _.keys(extend);
+    let searchKeys = _.concat( _.keys(changes), extendKeys );
+    _.extend( changes, _.zipObject(extendKeys, extendKeys));
+
+    return _.map(extendedData, item => {
+      let plucked =  _.partial(_.pick, item).apply(null, searchKeys);
+      return _.mapKeys(plucked, (value, key) => { return changes[key]; });
+    });
+
   }
 
   itemTapped(event, bicicleteria) {
