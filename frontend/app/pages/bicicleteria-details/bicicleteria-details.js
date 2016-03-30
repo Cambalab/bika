c29a82d48c0f99b955b2b063a3d5ebc6b6501bbe
smarbos <smarbos@gmail.com>
2016-03-30 16:50:42 -0300

    Add collapsable functionality to thee horarios button. Optimized white space

--- frontend/app/pages/bicicleteria-details/bicicleteria-details.js
+++ frontend/app/pages/bicicleteria-details/bicicleteria-details.js
@@ -5,13 +5,23 @@ import {Page, NavController, NavParams} from 'ionic-angular';
   templateUrl: 'build/pages/bicicleteria-details/bicicleteria-details.html'
 })
 export class BicicleteriaDetailsPage {
+
   static get parameters() {
     return [[NavController], [NavParams]];
   }
 
-  constructor(nav, navParams) {
+  constructor(nav, navParams, mostrarHorario) {
+
+    //inicialmente el horario esta oculto
+    var mostrarHorario = false;
+    this.mostrarHorario = mostrarHorario;
+    
     this.nav = nav;
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
+  }
+
+  toggleHorario(){
+    this.mostrarHorario = (this.mostrarHorario) ? false : true;
   }
 }
