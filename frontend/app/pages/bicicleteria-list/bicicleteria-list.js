d16123a1863f47f9afd0be84795163ef060efc1b
smarbos <smarbos@gmail.com>
2016-03-31 14:35:25 -0300

    Added modal-filters component and implemented it in the bicicleteria-list

--- frontend/app/pages/bicicleteria-list/bicicleteria-list.js
+++ frontend/app/pages/bicicleteria-list/bicicleteria-list.js
@@ -1,18 +1,20 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
+import {Page, NavController, NavParams, Modal} from 'ionic-angular';
 import {BicicleteriaDetailsPage} from '../bicicleteria-details/bicicleteria-details';
 import {BicicleteriaService} from '../../services/bicicleteria-service';
 import {Geolocation} from 'ionic-native';
+import {FilterPage, FiltersModal} from '../modal-filters/modal-filters';
 
 @Page({
   templateUrl: 'build/pages/bicicleteria-list/bicicleteria-list.html'
 })
+
 export class BicicleteriaListPage {
 
   static get parameters() {
     return [[NavController], [NavParams], [BicicleteriaService]];
   }
 
-  constructor(nav, navParams, bicicleteriaService) {
+  constructor(nav, navParams, bicicleteriaService, abrirModal) {
     this.nav = nav;
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
@@ -22,6 +24,11 @@ export class BicicleteriaListPage {
        console.log("Latitude: ", resp.coords.latitude);
        console.log("Longitude: ", resp.coords.longitude);
     })
+  }
+
+  openFiltersModal() {
+    let modal = Modal.create(FiltersModal);
+    this.nav.present(modal);
   }
 
   ngOnInit() {
